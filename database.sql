c91b91e65263a99f934a22bac0580c867d445045
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-23 18:54:18 +0500

    commit

--- database.sql
+++ database.sql
@@ -50,6 +50,19 @@ CREATE TABLE IF NOT EXISTS enrollments (
     FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS payments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    course_id INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    click_trans_id VARCHAR(100) NOT NULL,
+    merchant_trans_id VARCHAR(100) NOT NULL,
+    status ENUM('unpay', 'paid') DEFAULT 'unpay',
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
+);
+
 
 
 
