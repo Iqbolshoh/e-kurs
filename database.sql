32d2e325e3903c4a42a9479fbb8205c1a1dbfa1b
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-23 18:46:00 +0500

    commit

--- database.sql
+++ database.sql
@@ -27,6 +27,12 @@ CREATE TABLE IF NOT EXISTS active_sessions (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+
+
+
+
+
+
 -- admin and user (password: 'Iqbolsoh$7')
 INSERT INTO
     users (
